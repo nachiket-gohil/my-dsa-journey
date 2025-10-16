8dae4351037fdd7beec8c4bb4cca73a2b7a5e7bd
Nachiket Gohil <nachiketgohil185@gmail.com>
2025-10-16 11:15:28 +0530

    Updated comment in RotateArray.cpp

--- CppDSA/RotateArray.cpp
+++ CppDSA/RotateArray.cpp
@@ -1,5 +1,5 @@
 // Rotate Array
-// GFG Link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/rotate-array-by-n-elements-1587115621
+// GFG Problem Link : https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/rotate-array-by-n-elements-1587115621
 
 #include <iostream>
 #include <vector>
